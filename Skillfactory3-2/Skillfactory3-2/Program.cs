937d9715da5352adbca3479dd6b3dbf1a2c02ba9
Alexey <visotskiy.alexey@gmail.com>
2023-01-11 19:51:43 +0700

    Задание 3.2.4 Инициализация переменной

---
+++ Skillfactory3-2/Skillfactory3-2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Skillfactory3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Василий";
+            Console.WriteLine(MyName);
+        }
+    }
+}
