9b586bc6c3d41b054cb4cd0f644302d3e1805820
Alexey <visotskiy.alexey@gmail.com>
2023-01-11 19:57:46 +0700

    Задание 3.2.8

--- Skillfactory3-2/Skillfactory3-2/Program.cs
+++ Skillfactory3-2/Skillfactory3-2/Program.cs
@@ -4,9 +4,16 @@
     {
         static void Main(string[] args)
         {
-            string MyName;
-            MyName = "Василий";
+            const string MyName = "Jane";
+
             Console.WriteLine(MyName);
+
+            Console.WriteLine("\t Привет Мир");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\x23");
+            Console.ReadKey();
         }
     }
 }
